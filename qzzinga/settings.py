0c99b8488f249fda330d58134aed389f52346ed9
Ananya <65886348+Ananyaiitbhilai@users.noreply.github.com>
2021-04-25 23:10:47 +0530

    Update settings.py

--- qzzinga/settings.py
+++ qzzinga/settings.py
@@ -144,10 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files","static-root")
+STATICFILES_STORAGE ='whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files","media-root")
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
 
